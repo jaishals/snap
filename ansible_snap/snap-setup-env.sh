6719efb3cef96bee29f44f6027bd68d56a8dcf4a
jaishal <jaishal123@gmail.com>
2016-11-03 21:31:50 -0700

    Bug fixes

--- ansible_snap/snap-setup-env.sh
+++ ansible_snap/snap-setup-env.sh
@@ -1,4 +1,6 @@
 #!bin/bash
+export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
+export HADOOP_PREFIX=/opt/hadoop/default
 count=$(echo $PATH | grep -c "/opt/hadoop/default/bin")
 if [ $count -eq 0 ]; then
  export PATH=:"$PATH:/opt/hadoop/default/bin"
